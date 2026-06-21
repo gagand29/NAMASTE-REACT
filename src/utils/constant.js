20b8fcf610ee0a0d8060f95f300d2fc50228bdd6
Gagan D <47719899+gagand29@users.noreply.github.com>
2026-06-21 13:36:49 -0500

    architected and used hooks ... v2 (#1)

---
+++ src/utils/constant.js
@@ -0,0 +1,7 @@
+export const CDN_URL =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQZX3MKHbSkLGp1uqr9Ey4hrvfPSMTikp3tA&s";
+
+export const LOGO_URL =
+  "https://img.magnific.com/free-vector/food-shopping-logo-template-design_460848-10299.jpg?semt=ais_hybrid&w=740&q=80";
+
+
